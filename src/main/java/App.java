efcb147d9b17a3a421f6d7aac3ff72372f3592f2
RaulDuVrau <raulvilefran@gmail.com>
2019-03-01 22:38:01 -0300

    maven

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+
+
+
+public class App {
+	public static void main(String[] args) {
+		String[] resultado = ResultadoMegasena.obtemUltimoResultado();
+			for (String string : resultado) {
+				System.out.println(string +" ");
+			}
+	}
+}
